c13c505711c0e1557d5b87ee5bd7c05ff9edfda3
Aayushgarg007 <aayushgarg226@gmail.com>
2021-01-19 00:20:48 +0530

    add recommended section

---
+++ src/RecommendedVideos.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import './RecommendedVideos.css';
+import VideoCard from "./VideoCard";
+
+const RecommendedVideos = () => {
+  return (
+    <div className="recommendedVideos">
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard 
+          title="Become a web developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+          channel="Aayush"
+          image="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+        />
+        <VideoCard 
+          title="Become a web developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+          channel="Aayush"
+          image="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+        />
+        <VideoCard 
+          title="Become a web developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+          channel="Aayush"
+          image="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+        />
+        <VideoCard 
+          title="Become a web developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+          channel="Aayush"
+          image="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+        />
+        <VideoCard 
+          title="Become a web developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+          channel="Aayush"
+          image="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+        />
+        <VideoCard 
+          title="Become a web developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+          channel="Aayush"
+          image="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+        />
+        <VideoCard 
+          title="Become a web developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+          channel="Aayush"
+          image="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+        />
+        <VideoCard 
+          title="Become a web developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+          channel="Aayush"
+          image="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+        />
+        <VideoCard 
+          title="Become a web developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+          channel="Aayush"
+          image="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+        />
+        <VideoCard 
+          title="Become a web developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+          channel="Aayush"
+          image="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+        />
+        <VideoCard 
+          title="Become a web developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+          channel="Aayush"
+          image="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+        />
+        <VideoCard 
+          title="Become a web developer in 10 minutes"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+          channel="Aayush"
+          image="https://avatars0.githubusercontent.com/u/42891619?s=400&u=11a9777eb27754f44f0727e39949a894ba58f404&v=4"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default RecommendedVideos;
